92c4112c757404fa57e5a2b89404d0710ef5b579
arch <miniyu157@163.com>
2026-05-29 19:21:56 +0800

    feat! distros/alpine.sh 领先实现新的规范，准备迁移

--- distros/alpine.sh
+++ distros/alpine.sh
@@ -1,22 +1,62 @@
 #!/usr/bin/env bash
 
 # shellcheck shell=bash
-# shellcheck disable=SC2034
 
-DESC="Alpine Linux (dl-cdn.alpinelinux.org)"
-OPTION_ARCH=("aarch64" "x86_64")
-OPTION_VERSIONS=("latest-stable")
+set -euo pipefail
+shopt -s inherit_errexit 2> /dev/null || true
 
-distro_init() {
-    local yaml_url="https://dl-cdn.alpinelinux.org/alpine/${1}/releases/${2}/latest-releases.yaml"
+info() {
+    cat << EOF
+{
+  "desc": "Alpine Linux (dl-cdn.alpinelinux.org)",
+  "archs": ["aarch64", "x86_64"],
+  "versions": ["latest-stable"]
+}
+EOF
+}
+
+get() {
+    local version="${1}" arch="${2}"
+    [[ -z $version || -z $arch ]] && usage
+
+    local yaml_url="https://dl-cdn.alpinelinux.org/alpine/${version}/releases/${arch}/latest-releases.yaml"
     local yaml_text
-    yaml_text=$(curl -fsSL "$yaml_url" 2> /dev/null)
-    [[ -z $yaml_text ]] && return 1
-
+    yaml_text=$(curl -fsSL --connect-timeout 10 --max-time 30 "$yaml_url")
     local filename
     filename=$(echo "$yaml_text" | awk '/flavor: alpine-minirootfs/{found=1} found && /^  file: /{print $2; exit}')
+    local hash_val
+    hash_val=$(echo "$yaml_text" | awk '/flavor: alpine-minirootfs/{found=1} found && /^  sha256: /{print $2; exit}')
+    [[ -z $filename || -z $hash_val ]] && {
+        printf "Error: Unrecognized remote format: %s\n" "$yaml_url" >&2
+        return 1
+    }
+    local src
+    src="https://dl-cdn.alpinelinux.org/alpine/${version}/releases/${arch}/${filename}"
 
-    SRC="https://dl-cdn.alpinelinux.org/alpine/${1}/releases/${2}/${filename}"
-    HASH_VAL=$(echo "$yaml_text" | awk '/flavor: alpine-minirootfs/{found=1} found && /^  sha256: /{print $2; exit}')
-    HASH_CMD="sha256sum"
+    cat << EOF
+{
+  "src": "${src}",
+  "hash_val": "${hash_val}",
+  "hash_cmd": "sha256sum"
 }
+EOF
+}
+
+usage() {
+    echo "usage: $0 {info | get <version> <arch> }" >&2
+    exit 1
+}
+
+main() {
+    case "${1:-}" in
+        info)
+            info
+            ;;
+        get)
+            get "${2:-}" "${3:-}"
+            ;;
+        *) usage ;;
+    esac
+}
+
+[[ ${BASH_SOURCE[0]} == "${0}" ]] && main "$@"
